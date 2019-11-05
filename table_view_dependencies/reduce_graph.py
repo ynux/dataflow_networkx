b52a0054c96d5e3f9b8ff5654f15b849e811de8e
--show-origin <ynux@gmx.net>
2019-11-05 08:45:00 +0100

    remove pks with only 1 edge

--- table_view_dependencies/reduce_graph.py
+++ table_view_dependencies/reduce_graph.py
@@ -12,26 +12,45 @@ def reduce_object_graph_to_schemas(schemalist=['A','B'], prefix="test", add_colu
         nx.write_graphml(G_reduced, './data/output/{}_object_reduced.xml'.format(prefix))
     return G_reduced
 
-def reduce_pk_graph_to_schemas(schemalist=['A','B'], exclude_pk_list=[], prefix="test", add_tab_cols=False, write_file=False):   
+def reduce_pk_graph_to_schemas(schemalist=['A','B'], exclude_pk_list=[], prefix="test", only_connecting_pk=True, add_tab_cols=False, write_file=False):   
     """
     reduce an Primary Graph to the tables in the schemas in the schemalist. Some primary can be excluded (like valid_to in a time slice)
     """
     Gpk = build_pk_graph(add_tab_cols=add_tab_cols, prefix=prefix, write_file=False)
     tables_retain = [n for n,d in Gpk.nodes(data=True) if d['type'] == "BASE TABLE" and d['schema'] in schemalist]
     # now add the primary keys
-    neighbors=[]
+    neighbors=set()
     for table in tables_retain:
         for n in Gpk.neighbors(table):
-            neighbors.append(n)
-    pk_retain = [n for n,d in Gpk.nodes(data=True) if d['type'] == "PK" and n in neighbors and n not in exclude_pk_list]        
+            neighbors.add(n)
+
+    for pk in exclude_pk_list:
+        if pk in neighbors:
+            neighbors.remove(pk)
+
+    neighbor_dict = {}
+    for pk in neighbors:
+        pk_tables = 0
+        for nb in Gpk.neighbors(pk):
+            if nb in tables_retain:
+                pk_tables += 1
+        neighbor_dict[pk] = pk_tables
+
+    if only_connecting_pk:
+        # only retain pks with more than one edge    
+        neighbor_list =  [pk for pk in neighbor_dict if neighbor_dict[pk] > 1] 
+    else:
+        neighbor_list = list(neighbors)
+    
         
-    G_reduced = Gpk.subgraph(tables_retain + pk_retain)
+    G_reduced = Gpk.subgraph(tables_retain + neighbor_list)
     if write_file:
         nx.write_graphml(G_reduced, './data/output/{}_pk_reduced.xml'.format(prefix))
     return G_reduced
 
 if __name__ == '__main__':
-    G_reduced = reduce_object_graph_to_schemas()
-    print("Reduced Object Graph nodes: {0}, Reduced Object Graph edges: {1}".format(len(G_reduced.nodes()), len(G_reduced.edges())))
-    G_reduced = reduce_pk_graph_to_schemas(schemalist=['A','B'], prefix="test", write_file=False)
-    print("Reduced Primary Key Graph with lonely Tables nodes: {0}, Reduced Primary Key Graph with lonely Tables edges: {1}".format(len(G_reduced.nodes()), len(G_reduced.edges())))
+    #G_reduced = reduce_object_graph_to_schemas()
+    #print("Reduced Object Graph nodes: {0}, Reduced Object Graph edges: {1}".format(len(G_reduced.nodes()), len(G_reduced.edges())))
+    G_reduced = reduce_pk_graph_to_schemas(schemalist=["A"], exclude_pk_list=["VALID_TO", "DW_FROM_DATE", "DW_TO_DATE","VALID_FROM","VALIDFROM"], prefix="test", write_file=False)
+    print("Reduced Primary Key Graph nodes: {0}, Reduced Primary Key Graph edges: {1}".format(len(G_reduced.nodes()), len(G_reduced.edges())))
+    print("Reduced Primary Key Graph nodes: {0}, Reduced Primary Key Graph edges: {1}".format(len(G_reduced.nodes()), len(G_reduced.edges())))
